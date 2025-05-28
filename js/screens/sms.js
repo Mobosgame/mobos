879ed8df18298e599160ecd1c97a5ae04a67d05b
Mobosgame <andreoepta@gmail.com>
2025-05-29 01:49:11 +0400

    Update sms.js

--- js/screens/sms.js
+++ js/screens/sms.js
@@ -1,1 +1,6 @@
 
+// sms.js - теперь пустой, так как вся логика в router.js
+function initSms() {
+    // Дополнительная логика для экрана сообщений (если нужна)
+    console.log('SMS screen initialized');
+}
