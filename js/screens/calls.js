b4edc57d31e07ba30a4504a225bc3fd38c7e7357
Mobosgame <andreoepta@gmail.com>
2025-05-29 01:48:37 +0400

    Update calls.js

--- js/screens/calls.js
+++ js/screens/calls.js
@@ -1,1 +1,6 @@
 
+// calls.js - теперь пустой, так как вся логика в router.js
+function initCalls() {
+    // Дополнительная логика для экрана звонков (если нужна)
+    console.log('Calls screen initialized');
+}
