dd85bf7ecd1a3b30949120cba66063f5b4af0df4
Mobosgame <andreoepta@gmail.com>
2025-05-30 02:21:22 +0400

    Update calls.js

--- js/screens/calls.js
+++ js/screens/calls.js
@@ -1,4 +1,6 @@
-function initCalls() { // или initSms/initSettings для других экранов
-    document.querySelector('#calls-screen .close-btn').addEventListener('click', goBack);
-    // остальная инициализация
+// Замените содержимое файлов на:
+function initCalls() {
+    document.querySelector('#calls-screen .close-btn')?.addEventListener('click', goBack);
 }
+window.initCalls = initCalls;
+
