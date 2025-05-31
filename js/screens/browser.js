fa245011046ceebedc3b608c0c5ebd6d388123ff
Mobosgame <andreoepta@gmail.com>
2025-05-31 22:23:33 +0400

    Update browser.js

--- js/screens/browser.js
+++ js/screens/browser.js
@@ -1,5 +1,9 @@
-function initBrowser() {
-    // 1. Обработчик закрытия
-    document.querySelector('#browser-screen .close-btn').addEventListener('click', goBack);
-}
-window.initWallet = initWallet;
+<div id="browser-screen" class="app-screen">
+    <div class="app-header">
+        <h2>Браузер</h2>
+        <button class="close-btn">✕</button>
+    </div>
+    <div class="app-content">
+        <!-- Контент кошелька -->
+    </div>
+</div>
