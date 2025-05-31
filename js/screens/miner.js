ef8032979942adc38daf9b8cde5c342f7e572505
Mobosgame <andreoepta@gmail.com>
2025-06-01 00:14:14 +0400

    Update miner.js

--- js/screens/miner.js
+++ js/screens/miner.js
@@ -1,9 +1,5 @@
-<div id="miner-screen" class="app-screen">
-    <div class="app-header">
-        <h2>Майнер</h2>
-        <button class="close-btn">✕</button>
-    </div>
-    <div class="app-content">
-        <!-- Контент кошелька -->
-    </div>
-</div>
+function initMiner() {
+    // 1. Обработчик закрытия
+    document.querySelector('#miner-screen .close-btn').addEventListener('click', goBack);
+}
+window.initMiner = initMiner;
