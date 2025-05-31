b788de1897e737168a906899b3761c971d7263bc
Mobosgame <andreoepta@gmail.com>
2025-05-31 21:27:46 +0400

    Update browser.js

--- js/screens/browser.js
+++ js/screens/browser.js
@@ -2,3 +2,4 @@ function initBrowser() {
     // 1. Обработчик закрытия
     document.querySelector('#browser-screen .close-btn').addEventListener('click', goBack);
 }
+window.initWallet = initWallet;
