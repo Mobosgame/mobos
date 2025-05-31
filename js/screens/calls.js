c21e025e19e200e544a0e2137308c0b811f15aa3
Mobosgame <andreoepta@gmail.com>
2025-05-31 16:37:39 +0400

    Update calls.js

--- js/screens/calls.js
+++ js/screens/calls.js
@@ -1,12 +1,6 @@
 function initCalls() {
     // 1. Обработчик закрытия
     document.querySelector('#calls-screen .close-btn').addEventListener('click', goBack);
-    
-    // 2. Загрузка данных
-    // ...
-    
-    // 3. Настройка компонентов
-    // ...
 }
 
-window.initCalls = initCalls;
+
