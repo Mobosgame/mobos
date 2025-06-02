e3c3f9dcd07391a8a822f30811828564e68a8ff1
Mobosgame <andreoepta@gmail.com>
2025-06-02 23:05:25 +0400

    Update darkwall.js

--- js/screens/darkwall.js
+++ js/screens/darkwall.js
@@ -2,7 +2,7 @@
 function initDarkwall() {
     // Обработчик закрытия
     document.querySelector('#darkwall-screen .close-btn').addEventListener('click', () => {
-        resetGame();
+        
         goBack();
     });
 }
