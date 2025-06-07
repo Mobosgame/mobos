809101c4b175c54e966f12e04691e3fe9336ec97
Mobosgame <andreoepta@gmail.com>
2025-06-07 17:05:02 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -53,6 +53,14 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         };
 
+        // Добавляем обработчик для Darkwall Game
+      const darkwallBtn = document.getElementById('darkwall-btn');
+    if (darkwallBtn) {
+        darkwallBtn.addEventListener('click', () => {
+            showScreen('darkwall');
+        });
+    }
+
         // Настройка обработчиков для кнопок
         setupButton('settings-btn', 'settings');
         setupButton('calls-btn', 'calls');
@@ -67,11 +75,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const mainScreen = document.getElementById('main-screen');
         if (mainScreen) mainScreen.classList.remove('hidden');
 
-        // Добавляем обработчик для Darkwall Game
-        const darkwallBtn = document.getElementById('darkwall-btn');
-    if (darkwallBtn) {
-        darkwallBtn.addEventListener('click', () => {
-            showScreen('darkwall');
+        
         });
     }
 });
